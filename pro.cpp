553df93a2d435ac4e21b95acb2ebe9d4e049473e
dulajsan <dulaj.san@gmail.com>
2018-06-21 19:55:32 +0530

    load the images

--- pro.cpp
+++ pro.cpp
@@ -1,1 +1,24 @@
-dsad
+#include<opencv2/imgproc.hpp>
+#include<opencv2/highgui.hpp>
+#include<iostream>
+
+using namespace cv;
+
+
+int main(int argc,char** argv){
+	Mat img_1,img_2,img_1_gray,img_2_gray;
+
+        // -- load the images
+        img_1 = imread("Fish/img/0001.jpg");
+        img_2 = imread("Fish/img/0199.jpg");
+
+	// -- if image not found then show error message
+        if((! img_1.data) || (! img_2.data)){
+                std::cout << "image not found" << std::endl;
+                return -1;
+        }
+
+	
+	
+
+}
